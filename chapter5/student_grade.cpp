cb4f473a56edc417f8093ab5e61c08299f8b10f8
CarlosTheran <carlos.theran@upr.edu>
2024-02-01 16:14:20 -0500

    updated

--- chapter5/student_grade.cpp
+++ chapter5/student_grade.cpp
@@ -14,7 +14,9 @@ int main()
     ifstream input_file;
     int option;
     string given_name; 
+    bool flag = true;
     input_file.open("student_list.txt");
+    char answer = 'Y';
 
     if(!input_file)
     {
@@ -26,45 +28,84 @@ int main()
     while (!input_file.eof())
     {
         input_file >> students_name[i] >> first_test[i] >> second_test[i] >> third_test[i];
+        i++;
     }
     
-    cout<<right<<setfill('#')<<setw(20)<<" Menu "<<setfill('#')<<setw(20)<<"#"<<endl;
-    cout<<right<<"1. Find the student in the list."<<endl;
-    cout<<right<<"2. Compute the final grade of one student. "<<endl;
-    cout<<right<<"3. Compute the final grade of all student. "<<endl;
-    cout<<right<<"4. quit. "<<endl;
 
-    cout<<"Provide the selected option: ";
-    cin >> option;
-
-    switch (option)
+    while( answer == 'Y')
     {
-    case 1:
-        /* Find the student in the list */
-        cout<<"provide student's name:  ";
-        cin >> given_name;
-        for(int index=0; index< 4; index++)
-        {
-            if(given_name == students_name[index])
-            {
-                cout<<"first test: "<< first_test[index] <<", second test: "<<second_test[index]<<", third test: "<<third_test[index];
-                break;
-            }
-        }       
-        break;
-    case 2:
-        /* Compute the final grade of one student */
-        break;  
-    case 3:
-        /* Compute the final grade of all student */
-        break; 
-    case 4:
-        /* quit */
-        break;         
-    
-    default:
-        break;
-    }
 
+        cout<<right<<setfill('#')<<setw(20)<<" Menu "<<setfill('#')<<setw(20)<<"#"<<endl;
+        cout<<right<<"1. Find the student in the list."<<endl;
+        cout<<right<<"2. Compute the final grade of one student. "<<endl;
+        cout<<right<<"3. Compute the final grade of all students. "<<endl;
+        cout<<right<<"4. Quit. "<<endl;
+
+        cout<<"Provide the selected option: ";
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            /* Find the student in the list */
+            cout<<"provide student's name:  ";
+            cin >> given_name;
+            for(int index=0; index< 4; index++)
+            {
+                if(given_name == students_name[index])
+                {
+                    cout<<"first test: "<< first_test[index] <<", second test: "<<second_test[index]<<", third test: "<<third_test[index];
+                    flag = false;
+                    break;
+                }
+
+            }   
+            if(flag)
+            {
+                cout<<"The student "<<given_name<<" is not in the list."<<endl; 
+            }  
+    
+            break;
+        case 2:
+            /* Compute the final grade of one student */
+            cout<<"provide student's name:  ";
+            cin >> given_name;
+            for(int index=0; index< 4; index++)
+            {
+                if(given_name == students_name[index])
+                {
+                    cout<<"Final grade: "<< (first_test[index] + second_test[index] + third_test[index])/3;
+                    flag = false;
+                    break;
+                }
+
+            }   
+            if(flag)
+            {
+                cout<<"The student "<<given_name<<" is not in the list."<<endl; 
+            }  
+            break;  
+        case 3:
+            /* Compute the final grade of all student */
+            for(int i=0; i<4; i++)
+            {
+            cout<<students_name[i]<<"--- Final grade: "<< (first_test[i] + second_test[i] + third_test[i])/3<<endl; 
+            }
+            break; 
+        case 4:
+            /* quit */
+            cout<<"Exit the program."<<endl;
+            exit(1);
+            break;         
+        
+        default:
+            break;
+        }
+
+     cout<<"\nDo you want to do any other task, do Y or N: ";
+     cin >> answer;
+
+    }
+    
     return 0;
 }
